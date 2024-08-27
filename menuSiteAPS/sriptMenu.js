2c343ee4da81dfcae5359b346b4b53c64798c217
Caua Mariz DEV <marizcaua88@gmail.com>
2024-08-27 14:12:05 -0300

    Update sriptMenu.js

--- menuSiteAPS/sriptMenu.js
+++ menuSiteAPS/sriptMenu.js
@@ -12,25 +12,29 @@ function VerMenu() {
         menu.style.fontFamily = "Arial"
         menu.style.paddingLeft = "1%"
 
-        let home='<a href="#" class="home"><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/home.png" alt="home"/></a>' 
+        let home = '<a href="#" class="home"><img width="40" height="40" src="https://img.icons8.com/ios-glyphs/48/FFFFFF/home.png" alt="home" /></a>'
 
-        let carrinho='<a href="#" class="carrinho"><img src="https://img.icons8.com/fluency-systems-regular/48/shopping-cart--v1.png" alt="" width="40" height="40"></a>' 
-        
-        let login='<a href="#" class="login"><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/user-male-circle.png" alt="user-male-circle"/></a>' 
+        let carrinho = '<a href="#" class="carrinho"><img width="40" height="40" src="https://img.icons8.com/ios-glyphs/40/FFFFFF/shopping-cart.png" alt="shopping-cart"/></a>'
 
-        var text=menu.innerHTML = home + carrinho + login
-        
-        
-        
-        text.id="campoMenu"
-       
+        let login = '<a href="#" class="login"><img width="40" height="40" src="https://img.icons8.com/ios-glyphs/48/FFFFFF/guest-male.png" alt="guest-male"/></a>'
+
+        let osso = '<a href="#" class="osso"><img width="40" height="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/dog-bone.png" alt="dog-bone"/></a>'
+
+        let gato = '<a href="#" class="gato"><img width="40" height="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/pet-commands-stay.png" alt="pet-commands-stay"/></a>'
+
+        let config = '<a href="#" class="config"><img width="40" height="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/settings-3.png" alt="settings-3"/></a>'
+
+
+
+        var text = menu.innerHTML = home + gato + osso + carrinho + login + config
+
+        text.id = "campoMenu"
     }
     else {
         menu.classList.remove("AddMenu")
         menu.classList.add("RmvMenu")
         BtnMenu.style.marginLeft = "0"
         BtnMenu.style.marginTop = "0"
-        text.style.opacity="0"
-        
     }
 }
+
