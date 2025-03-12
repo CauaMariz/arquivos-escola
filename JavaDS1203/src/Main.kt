eaa0da9f57441445cc8e46dbc15271726672788d
Caua Mariz DEV <marizcaua88@gmail.com>
2025-03-12 12:49:11 -0300

    Add files via upload

---
+++ JavaDS1203/src/Main.kt
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var nome = "Rogeriano"
+    var idade = 27
+    var letra = 'R'
+    var altura = 1.89
+    var peso = 84
+
+
+    print("Nome: " + nome + "\nIdade: " + idade + "\nprimeiraletra                                                                                            ")
+
+
+//  println(nome)
+//    println(idade)
+//    println(letra)
+//    println(altura)
+//    println(peso)
+
+
+
+}
---
+++ JavaDS1203/src/Teste2.java
@@ -0,0 +1,7 @@
+
+public class Teste2 {
+    public static void main(String[] args){
+        Calculadora x = new Calculadora();
+        x.menu();
+      }
+}
