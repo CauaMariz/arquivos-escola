4572eb91aa53437dfedc02e9b4eb12b09f3d2f29
Caua Mariz DEV <marizcaua88@gmail.com>
2025-08-29 15:33:12 -0300

    Create home.php

---
+++ PW/AulaPW.08.08.25/php/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="cadastro.php" method="post">
+
+        <p class="title">Cadastra-se</p>
+
+        <input type="text" placeholder="Nome: " required name="nomeAluno">
+        <input type="email" placeholder="Email: " required name="emailAluno">
+
+        <input type="number" placeholder="RM: " required name="rmAluno">
+        <input type="number" placeholder="CPF: " required name="cpfAluno">
+
+        <ul class="lista">
+            <li class="li1">Manter-me logado</li>
+            <input type="checkbox">
+            <li class="li2">Já tem uma conta?</li>
+        </ul>
+
+        <button class="btnCadastrar" type="submit">Cadastrar</button>
+    </form>
+</body>
+
+</html>
