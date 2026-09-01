638771f0d8c4b77cb066b3168df02ab8ab61b2c3
Caua Mariz DEV <marizcaua88@gmail.com>
2026-09-01 12:44:34 -0300

    Create paginaFotos.php

---
+++ PW/ProjetoPW3.18.08.26/loja/paginaFotos.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style/stylePaginaFotos.css">
+</head>
+
+<body>
+    <section class="campo-fotos">
+        <div class="foto1">
+            <div class="fundoFoto">
+                <img src="imagens/06e8b56683559244dd0a2db21416b019-removebg-preview.png" alt="">
+            </div>
+            <div class="informacoes">
+                <div class="tituloFoto">Pão de forma</div>
+                <div class="preco">R$ 5,00</div>
+            </div>
+        </div>
+        <div class="foto2">
+            <div class="fundoFoto">
+                <img src="imagens/06e8b56683559244dd0a2db21416b019-removebg-preview.png" alt="">
+            </div>
+            <div class="informacoes">
+                <div class="tituloFoto">Pão de forma</div>
+                <div class="preco">R$ 5,00</div>
+            </div>
+        </div>
+        <div class="foto3">
+            <div class="fundoFoto">
+                <img src="imagens/06e8b56683559244dd0a2db21416b019-removebg-preview.png" alt="">
+            </div>
+            <div class="informacoes">
+                <div class="tituloFoto">Pão de forma</div>
+                <div class="preco">R$ 5,00</div>
+            </div>
+        </div>
+        <div class="foto4">
+            <div class="fundoFoto">
+                <img src="imagens/06e8b56683559244dd0a2db21416b019-removebg-preview.png" alt="">
+            </div>
+            <div class="informacoes">
+                <div class="tituloFoto">Pão de forma</div>
+                <div class="preco">R$ 5,00</div>
+            </div>
+        </div>
+        <div class="foto5">
+            <div class="fundoFoto">
+                <img src="imagens/06e8b56683559244dd0a2db21416b019-removebg-preview.png" alt="">
+            </div>
+            <div class="informacoes">
+                <div class="tituloFoto">Pão de forma</div>
+                <div class="preco">R$ 5,00</div>
+            </div>
+        </div>
+        <div class="foto6">
+            <div class="fundoFoto">
+                <img src="imagens/06e8b56683559244dd0a2db21416b019-removebg-preview.png" alt="">
+            </div>
+            <div class="informacoes">
+                <div class="tituloFoto">Pão de forma</div>
+                <div class="preco">R$ 5,00</div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
